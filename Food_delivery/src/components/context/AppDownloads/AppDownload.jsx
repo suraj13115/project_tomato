391ac90538215081ed6d689d42f3507021bd3bb1
suraj13115 <kumarsurajgupta13115@gmail.com>
2025-09-08 14:32:45 +0530

    Fix AppDownload component and resolve import issues

---
+++ Food_delivery/src/components/context/AppDownloads/AppDownload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppDownload.css';
+
+const AppDownload = () => {
+  return (
+    <div>
+      {/* Add your app download content here */}
+    </div>
+  );
+};
+
+export default AppDownload;
